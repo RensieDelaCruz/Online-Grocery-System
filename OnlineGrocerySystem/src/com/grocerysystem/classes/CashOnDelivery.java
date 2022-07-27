bbf8b120edf01f546aac606d861ace9a96a183b2
Rensie Dela Cruz <100082056+Bachuchu@users.noreply.github.com>
2022-07-26 20:20:47 -0500

    Implemented the abstract method from abstract parent class

--- OnlineGrocerySystem/src/com/grocerysystem/classes/CashOnDelivery.java
+++ OnlineGrocerySystem/src/com/grocerysystem/classes/CashOnDelivery.java
@@ -1,5 +1,15 @@
 package com.grocerysystem.classes;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Calendar;
+
 public class CashOnDelivery extends Payment {
 
 	private double amountRemainingBalance, amountRecieved, amountChange;
@@ -7,14 +17,45 @@ public class CashOnDelivery extends Payment {
 	@Override
 	public void collectPayment() {
 
+	public CashOnDelivery(double amount) {
+		this.amount = amount;
+		this.date = LocalDate.now();
+		this.timeStamp = new Timestamp(cal.getTimeInMillis());
 	}
 
-	public double calculateRemainingBalance() {
-		return 0.0;
-	}
-	
-	public double calculateChange() {
-		return 0.0;
+	@Override
+	public int collectPayment() {
+		try (Connection conn = DatabaseConnect.getConnection()) {
+			// first, insert payment info into payment table to generate an auto-increment
+			// payment id that will bne use to other tables
+			String insertPayment = "INSERT INTO payment(payment_type, date, time_stamp) VALUES (?,?,?)";
+			PreparedStatement stmt1 = conn.prepareStatement(insertPayment);
+			stmt1.setString(1, "M");
+			stmt1.setDate(2, Date.valueOf(date));
+			stmt1.setString(3, sdf.format(timeStamp));
+			stmt1.execute();
+
+			// get the the generated payment id
+			String getPaymentID = "SELECT payment_id FROM payment WHERE payment_type = ? AND date = ? AND time_stamp = ? ";
+			PreparedStatement stmt2 = conn.prepareStatement(getPaymentID);
+			stmt2.setString(1, "M");
+			stmt2.setDate(2, Date.valueOf(date));
+			stmt2.setString(3, sdf.format(timeStamp));
+			ResultSet rs = stmt2.executeQuery();
+			while (rs.next()) {
+				paymentID = rs.getInt("payment_id");
+			}
+			
+			String insertQuery = "INSERT INTO payment_cash(payment_id, amount) VALUES(?,?)";
+			PreparedStatement stmt3 = conn.prepareStatement(insertQuery);
+			stmt3.setInt(1, paymentID);
+			stmt3.setDouble(2, amount);
+			stmt3.execute();
+
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+		return paymentID;
 	}
 
 }
